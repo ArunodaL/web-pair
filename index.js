387087adb19b2b25f96cb33f0b5ca34712cce2f7
ArunodaL <arunoddisanayake@gmail.com>
2025-05-23 05:48:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require("express");
+cconst express = require("express");
 const app = express();
-__path = process.cwd();
-const bodyParser = require("body-parser");
+const __path = process.cwd();
 const PORT = process.env.PORT || 8000;
+
 let code = require("./pair");
 require("events").EventEmitter.defaultMaxListeners = 500;
+
+// Body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.use("/code", code);
 
-app.use("/", async (req, res, next) => {
+// Fallback route (serve HTML)
+app.use("/", async (req, res) => {
   res.sendFile(__path + "/pair.html");
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Start server
 app.listen(PORT, () => {
   console.log(`⏩ Server running on http://localhost:` + PORT);
 });
